0413c5852ed6a5dadde591d22877622b99ba59c5
akielaries <akiel@akiel.org>
2025-06-12 22:46:10 -0700

    add mkupdater rough plan file

---
+++ mkupdater.cpp
@@ -0,0 +1,10 @@
+/**
+ * @brief Make an update package from a manifest
+ */
+
+
+// include some common header here. remember, we're packing a bunch of binary
+// data together into 1 blob. we need to be able to reverse that on the actual
+// hardware when applying this update
+
+// bulk of the instructions can be seen in the README
