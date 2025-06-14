cb597b435987f11e5517e1102d5f471d32769305
akielaries <akiel@akiel.org>
2025-06-14 15:48:19 -0700

    gitignore

--- modules/aam/src/dac_ex.c
+++ modules/aam/src/dac_ex.c
@@ -3,24 +3,27 @@
 #include "chprintf.h"
 #include <math.h>
 
-
 #define SAMPLES           100
 #define AMPLITUDE         2047
 #define OFFSET            2048
-#define SYSTEM_FREQ_HZ    1000000  // 1 MHz virtual timer base
 
 
 static dacsample_t sine_table[SAMPLES];
-
+static dacsample_t stereo_table[SAMPLES * 2];
 
 BaseSequentialStream* chp = (BaseSequentialStream*)&SD5;
 
+static const DACConversionGroup dac_grp_cfg = {
+  .num_channels = 2,
+  .end_cb       = NULL,
+  .error_cb     = NULL,
+  .trigger      = true
+};
 
 static const DACConfig dac_cfg = {
   .init     = 2048U,
   .datamode = DAC_DHRM_12BIT_RIGHT,
 };
-
 
 static const SerialConfig uart5_cfg = {
   .speed = 1000000,
@@ -29,15 +32,6 @@ static const SerialConfig uart5_cfg = {
   .cr3   = 0,
 };
 
-
-void delay_us_approx(uint32_t us) {
-  // Tweak this loop constant for your MCU speed
-  for (volatile uint32_t i = 0; i < (us * 10); i++) {
-    __asm__ volatile ("nop");
-  }
-}
-
-// Initialize sine table
 void init_sine_table(void) {
   for (int i = 0; i < SAMPLES; i++) {
     float theta = 2.0f * M_PI * i / SAMPLES;
@@ -45,79 +39,73 @@ void init_sine_table(void) {
   }
 }
 
-/*
-// Output sine wave of desired frequency (in Hz)
-void play_sine_wave(float frequency_hz) {
-  int i = 0;
-  // This is how often each sample must be output (us)
-  float sample_interval_us = 1000000.0f / (frequency_hz * SAMPLES);
-  systime_t period_ticks = TIME_US2I((uint32_t)sample_interval_us);
+void init_stereo_sine_table(void) {
+  for (int i = 0; i < SAMPLES; i++) {
+    float theta = 2.0f * M_PI * i / SAMPLES;
+    dacsample_t sample = (dacsample_t)(AMPLITUDE * sinf(theta) + OFFSET);
 
-  systime_t next = chVTGetSystemTimeX();
-  while (true) {
-    dacPutChannelX(&DACD1, 0, sine_table[i]);
-    dacPutChannelX(&DACD2, 0, sine_table[i]);
-    i = (i + 1) % SAMPLES;
-    next += period_ticks;
-    chThdSleepUntil(next);
-  }
-}
-*/
-void play_sine_wave(float frequency_hz) {
-  int i = 0;
-  float sample_interval_us = 1000000.0f / (frequency_hz * SAMPLES);
-
-  while (true) {
-    //chprintf(chp, "writing %d to DAC1\r\n", sine_table[i]);
-    //chprintf(chp, "writing %d to DAC2\r\n", sine_table[i]);
-    dacPutChannelX(&DACD1, 0, sine_table[i]);
-    dacPutChannelX(&DACD2, 0, sine_table[i]);
-    i = (i + 1) % SAMPLES;
-    delay_us_approx((uint32_t)sample_interval_us);
+    stereo_table[2 * i]     = sample; // DAC CH1 (Left)
+    stereo_table[2 * i + 1] = sample; // DAC CH2 (Right)
   }
 }
 
+void setup_tim6_for_dac(uint32_t sample_rate_hz) {
+  rccEnableTIM6(true);  // Enable clock for TIM6
+  uint32_t timer_clk = STM32_TIMCLK1;
+  uint32_t arr = timer_clk / sample_rate_hz;
 
-void dac_write(int dac, int16_t val) {
-  switch(dac) {
-    case 1:
-      dacPutChannelX(&DACD1, 0, val);
-      break;
-    case 2:
-      dacPutChannelX(&DACD2, 0, val);
-      break;
-  }
+  TIM6->PSC = 0;
+  TIM6->ARR = arr - 1;
+  TIM6->CR2 = TIM_CR2_MMS_1;   // TRGO on update event
+  TIM6->CR1 = TIM_CR1_CEN;
 }
 
 int main(void) {
   halInit();
   chSysInit();
 
-  // UART Debug setup
+  // UART Debug
   palSetPadMode(GPIOC, GPIOC_PIN12, PAL_MODE_ALTERNATE(8)); // USART5 TX
   palSetPadMode(GPIOD, GPIOD_PIN2,  PAL_MODE_ALTERNATE(8)); // USART5 RX
   sdStart(&SD5, &uart5_cfg);
-  chprintf(chp, "start of program...\r\n");
+  chprintf(chp, "Start of DMA sine wave generator...\r\n");
 
   // GPIO Setup
-  palSetPadMode(GPIOA, GPIOA_PIN3, PAL_STM32_MODE_OUTPUT);  // LED
-  palSetPadMode(GPIOA, GPIOA_PIN4, PAL_STM32_MODE_ANALOG);  // DAC1_CH1
-  palSetPadMode(GPIOA, GPIOA_PIN5, PAL_STM32_MODE_ANALOG);  // DAC1_CH2
+  palSetPadMode(GPIOA, GPIOA_PIN3, PAL_STM32_MODE_OUTPUT);   // LED
+  palSetPadMode(GPIOA, GPIOA_PIN4, PAL_STM32_MODE_ANALOG);  // DAC1 CH1
+  palSetPadMode(GPIOA, GPIOA_PIN5, PAL_STM32_MODE_ANALOG);
 
-  for (uint8_t i = 0; i < 10; i++) {
+  // Blink indicator
+  for (uint8_t i = 0; i < 5; i++) {
     palSetPad(GPIOA, GPIOA_PIN3);
-    chThdSleepMilliseconds(50);
+    chThdSleepMilliseconds(100);
     palClearPad(GPIOA, GPIOA_PIN3);
-    chThdSleepMilliseconds(50);
+    chThdSleepMilliseconds(100);
   }
 
-  init_sine_table();           // generate waveform table
-  dacStart(&DACD1, &dac_cfg);  // start DAC channels
-  dacStart(&DACD2, &dac_cfg);
+  init_stereo_sine_table();
 
-  float frequency = 100.0f;
-  play_sine_wave(frequency);
+  // Start DAC driver
+  dacStart(&DACD1, &dac_cfg);
 
-  return 0; // Unreachable
+  // Setup timer for DAC trigger (TIM6 TRGO)
+  float freq = 2000.0f;
+  int sample_rate = (int)(freq * SAMPLES);  // 200 * 100 = 20 kHz
+  setup_tim6_for_dac(sample_rate);
+
+  // Enable DAC channel trigger and DMA (trigger source TIM6 TRGO)
+  DAC1->CR = 0;  // Clear config
+  DAC1->CR |= DAC_CR_TEN1 | DAC_CR_TSEL1_2 | DAC_CR_TSEL1_1 | DAC_CR_TSEL1_0 | DAC_CR_EN1 | DAC_CR_DMAEN1;
+  DAC1->CR |= DAC_CR_TEN2 | DAC_CR_TSEL2_2 | DAC_CR_TSEL2_1 | DAC_CR_TSEL2_0 | DAC_CR_EN2 | DAC_CR_DMAEN2;
+
+  chprintf(chp, "Configured %d Hz sine wave, SR = %d\r\n", (int)freq, sample_rate);
+
+  // Start DMA transfer (stereo interleaved)
+  dacStartConversion(&DACD1, &dac_grp_cfg, stereo_table, SAMPLES);
+
+  while (true) {
+    chThdSleepMilliseconds(1000);
+  }
+
+  return 0;
 }
-
