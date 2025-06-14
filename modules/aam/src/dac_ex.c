f800339e2118b62f4f0cc821a3a04acaf8043de1
akielaries <akiel@akiel.org>
2025-06-14 12:00:07 -0700

    this creates sound on the DAC pins

--- modules/aam/src/dac_ex.c
+++ modules/aam/src/dac_ex.c
@@ -13,10 +13,10 @@ static dacsample_t right_wave[SAMPLES];
 
 
 static const DACConfig dac_cfg = {
-  .init     = 2047U,
+  .init     = 2048U,
   .datamode = DAC_DHRM_12BIT_RIGHT,
   //.cr       = 0,
-  .cr       = DAC_CR_TEN1 | DAC_CR_TEN2
+  //.cr       = DAC_CR_TEN1 | DAC_CR_TEN2
   //.cr       = DAC_CR_TSEL1_0 | DAC_CR_TEN1 |  // DAC1_CH1 trigger
   //            DAC_CR_TSEL2_0 | DAC_CR_TEN2    // DAC1_CH2 trigger
 };
@@ -80,6 +80,9 @@ int main(void) {
   // Configure serial TX/RX for debugging
   palSetPadMode(GPIOC, GPIOC_PIN12, PAL_MODE_ALTERNATE(8)); // USART5 TX
   palSetPadMode(GPIOD, GPIOD_PIN2,  PAL_MODE_ALTERNATE(8)); // USART5 RX
+  palSetPadMode(GPIOG, GPIOG_PIN3, PAL_MODE_INPUT); // ext push button
+  palSetPadMode(GPIOA, GPIOA_PIN3, PAL_STM32_MODE_OUTPUT); // blue LED?
+
   palSetPadMode(GPIOA, GPIOA_PIN4, PAL_STM32_MODE_ANALOG); // DAC1_CH1
   palSetPadMode(GPIOA, GPIOA_PIN5, PAL_STM32_MODE_ANALOG); // DAC1_CH2
 
@@ -99,16 +102,26 @@ int main(void) {
   init_stereo_wave();
   dacStart(&DACD1, &dac_cfg);
   dacStart(&DACD2, &dac_cfg);
+/*
   setup_tim6_for_dac();
   dacStartConversion(&DACD1, &single_channel_cfg, left_wave, SAMPLES);
   dacStartConversion(&DACD2, &single_channel_cfg, right_wave, SAMPLES);
-
+*/
   // debug DAC voltage
   // dacPutChannelX(&DACD1, 0, 1024);
   // dacPutChannelX(&DACD2, 0, 3072);
 
-  //dac_write(1, 2048); // Write mid-scale to PA4
-  //dac_write(2, 4095); // Write some value to PA5
+//  dac_write(1, 4095); // Write mid-scale to PA4
+//  dac_write(2, 4095); // Write some value to PA5
+int i = 0;
+while (true) {
+  float theta = 2.0f * M_PI * i / 100.0f;
+  int16_t val = (int16_t)(2047 * sinf(theta) + 2048);
+  dac_write(1, val);
+  dac_write(2, val);
+  i = (i + 1) % 100;
+  //chThdSleepMilliseconds(1); // ~1 kHz update rate
+}
 
   // Debug loop
   int counter = 0;
