067a556949fef3e2795cf186693056f21d966f8f
akielaries <akiel@akiel.org>
2025-06-17 23:10:18 -0700

    fighting the build

--- modules/core/bsp/src/main.c
+++ modules/core/bsp/src/main.c
@@ -1,16 +1,7 @@
 /**
- * @brief BSP main entry point
+ * @brief BSP spoof main entry point
  */
-#include "ch.h"
-#include "hal.h"
-
-// declare a symbol that must be defined by the application
-extern void app_main(void);
-
 int main(void) {
-  halInit();
-  chSysInit();
-
-  app_main();  // application takes over from here
+  return 0;
 }
 
