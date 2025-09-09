771ba4a314a8f23b7a56135fdbf40a854ab93e50
akielaries <akiel@akiel.org>
2025-09-08 20:08:29 -0700

    cmake-ifying chibi myself

--- toolchain/chibiOS_common.cmake
+++ toolchain/chibiOS_common.cmake
@@ -2,7 +2,7 @@ if(NOT DEFINED CHIBIOS_ROOT)
   set(CHIBIOS_ROOT "${CMAKE_SOURCE_DIR}/../../import/ChibiOS" CACHE PATH "Path to ChibiOS sources")
 endif()
 
-function(add_chibios_rt_library target)
+function(add_chibios_common_library target)
   cmake_parse_arguments(ARG "" "CHIBIOS_ROOT;TYPE" "" ${ARGN})
 
   if(ARG_CHIBIOS_ROOT)
@@ -15,20 +15,22 @@ function(add_chibios_rt_library target)
     set(ARG_TYPE "STATIC")
   endif()
 
-  file(GLOB _rt_sources "${_chibios_root}/os/rt/src/*.c")
+  file(GLOB _common_sources "${_chibios_root}/os/rt/src/*.c")
 
   if(ARG_TYPE STREQUAL "OBJECT")
-    add_library(${target} OBJECT ${_rt_sources})
+    add_library(${target} OBJECT ${_common_sources})
   else()
-    add_library(${target} STATIC ${_rt_sources})
+    add_library(${target} STATIC ${_common_sources})
   endif()
 
   target_include_directories(${target} PUBLIC
     "${_chibios_root}/os/rt/include"
     "${_chibios_root}/os/oslib/include"
+    "${_chibios_root}/os/common/oop/include"
+    "${_chibios_root}/os/license"
     "${_chibios_root}/os/oslib/include"
-    "${_chibios_root}/os/hal/osal/rt-nil"
   )
 
   set_target_properties(${target} PROPERTIES POSITION_INDEPENDENT_CODE OFF)
 endfunction()
+
