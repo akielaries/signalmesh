fdba0ee3a6b081b17ad34c6a5b9406cdf5b3be41
akielaries <akiel@akiel.org>
2026-03-01 03:04:08 +0000

    fpgapwr update

--- tools/fpgapwr.py
+++ tools/fpgapwr.py
@@ -3,25 +3,23 @@
 import RPi.GPIO as GPIO
 import time
 
-# Use GPIO pin 17 (change to whichever pin you connected)
 CONTROL_PIN = 26
 
-# Setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(CONTROL_PIN, GPIO.OUT)
-GPIO.output(CONTROL_PIN, GPIO.LOW)  # Start with button released
-
-print("Toggling FPGA power...")
-print("Holding 'button' for 2 seconds.")
-
-# Press the button (turn transistor on)
-GPIO.output(CONTROL_PIN, GPIO.HIGH)
-
-# Wait 2 seconds
-time.sleep(3)
-
-# Release the button
+# start w button "released"
 GPIO.output(CONTROL_PIN, GPIO.LOW)
 
-print("Done.")
+print("Toggling FPGA power...")
+print("Holding for 3 seconds...")
+
+# press the button (turn transistor on)
+GPIO.output(CONTROL_PIN, GPIO.HIGH)
+
+time.sleep(3)
+
+# release the button
+GPIO.output(CONTROL_PIN, GPIO.LOW)
+
+print("Done....")
 GPIO.cleanup()
