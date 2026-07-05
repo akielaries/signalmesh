a56690a6df433e90fc5a7913492a16a952e2bf17
akielaries <akiel@akiel.org>
2026-07-05 08:45:14 -0700

    updater

---
+++ tools/fw_update/update.cpp
@@ -0,0 +1,20 @@
+// x86 application to update the signalmesh components over USB->UART
+
+
+#include <iostream>
+
+
+
+int main (void) {
+  std::cout << "hello?\n";
+
+  // options here? help menu, etc
+
+
+  // run like:
+  // ./update --dev /path/to/dev --component [APM | ACM] --file foo.bin
+
+
+
+  return 0;
+}
